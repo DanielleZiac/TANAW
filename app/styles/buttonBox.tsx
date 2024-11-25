1809cf6304b549dde8d335247845365eca70f332
JonBabao <130745282+JonBabao@users.noreply.github.com>
2024-11-25 12:21:22 +0800

    Update buttonBox.tsx

--- app/styles/buttonBox.tsx
+++ app/styles/buttonBox.tsx
@@ -1,22 +1,22 @@
-// src/components/ButtonBox.tsx
 import React from 'react';
 
 interface ButtonBoxProps {
   children: React.ReactNode;
-  style?: React.CSSProperties;
   onClick?: () => void;
-  type?: 'button' | 'submit' | 'reset';
+  style?: React.CSSProperties; 
 }
 
-const ButtonBox: React.FC<ButtonBoxProps> = ({ children, onClick }) => {
+const ButtonBox: React.FC<ButtonBoxProps> = ({ children, onClick, style }) => {
   return (
     <button
+      type="button"
       onClick={onClick}
-      className="bg-cGray text-gray-700 rounded-lg py-4 px-6 w-full shadow-lg flex items-center justify-center space-x-4 hover:bg-gray-100 focus:outline-none"
+      className="bg-[#e4e9ef] text-gray-700 rounded-lg py-4 px-6 w-full shadow-lg flex items-center justify-center space-x-4 hover:bg-gray-100 focus:outline-none"
       style={{
         boxShadow:
-          "0px 8px 16px rgba(0, 0, 0, 0.3), 0px 4px 6px rgba(0, 0, 0, 0.1), -4px 0px 6px rgba(0, 0, 0, 0.1), " + // Original shadows
-          "0px -6px 10px rgba(255, 255, 255, 0.2)", // Outer white shadow on top
+          "0px 8px 16px rgba(0, 0, 0, 0.3), 0px 4px 6px rgba(0, 0, 0, 0.1), -4px 0px 6px rgba(0, 0, 0, 0.1), " +
+          "0px -6px 10px rgba(255, 255, 255, 0.2)",
+        ...style, 
       }}
     >
       {children}
