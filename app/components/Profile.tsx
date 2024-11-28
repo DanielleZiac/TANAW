5bf7da18c46599968d1684c6c55885a1a352ae46
JonBabao <jonendrickb@gmail.com>
2024-11-28 21:50:16 +0800

    Make Profile Responsive

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -5,22 +5,23 @@ interface ProfilePopupProps {
 }
 const ProfilePopup: React.FC<ProfilePopupProps> = ({ closePopup }) => {
   return (
-    <TextBoxPanel>
+    <div className="px-10 md:px-0">
+<TextBoxPanel>
       <div className="flex flex-col">
         <div className="flex flex-row gap-8">
           <div
-            className="rounded-3xl w-[150px] h-[150px] flex items-center justify-center"
+            className="rounded-3xl w-[150px] h-[150px] flex-shrink-0 flex items-center justify-center"
             style={{
               backgroundColor: "white",
               boxShadow: "inset 0px 8px 20px rgba(0, 0, 0, 0.4)",
             }}
           ></div>
-          <div className="flex flex-col gap-2">
-            <p className="font-bold text-2xl text-gray-800">Jon Endrick Babao</p>
-            <p className="font-semibold text-2xl text-gray-800">22-01675</p>
+          <div className="flex flex-col gap-2 justify-center">
+            <p className="font-bold lg:text-2xl text-gray-800">Jon Endrick Babao</p>
+            <p className="font-semibold lg:text-2xl text-gray-800">22-01675</p>
             
-              <div className="flex flex-row items-center gap-8 mt-4">
-                <div className="flex flex-col text-lg">
+              <div className="flex flex-row items-center gap-8 mt-4 hidden sm:block">
+                <div className="flex flex-col lg:text-lg">
                   <p className="font-bold text-gray-600">Batangas State University</p>
                   <p className="font-bold text-gray-600">CICS</p>
                 </div>
@@ -35,7 +36,16 @@ const ProfilePopup: React.FC<ProfilePopupProps> = ({ closePopup }) => {
           </button>
         </div>
       </div>
+      <div className="flex flex-row items-center gap-8 mt-4 sm:hidden">
+                <div className="flex flex-col lg:text-lg">
+                  <p className="font-bold text-gray-600">Batangas State University</p>
+                  <p className="font-bold text-gray-600">CICS</p>
+                </div>
+              </div>
+      
     </TextBoxPanel>
+    </div>
+    
   );
 };
 export default ProfilePopup;
