3de477b81751cdb7d1b618e332c9f8c5a2d86d65
JonBabao <jonendrickb@gmail.com>
2024-11-28 21:53:05 +0800

    Fix Profile Code Structure

--- app/components/Profile.tsx
+++ app/components/Profile.tsx
@@ -6,44 +6,44 @@ interface ProfilePopupProps {
 const ProfilePopup: React.FC<ProfilePopupProps> = ({ closePopup }) => {
   return (
     <div className="px-10 md:px-0">
-<TextBoxPanel>
-      <div className="flex flex-col">
-        <div className="flex flex-row gap-8">
-          <div
-            className="rounded-3xl w-[150px] h-[150px] flex-shrink-0 flex items-center justify-center"
-            style={{
-              backgroundColor: "white",
-              boxShadow: "inset 0px 8px 20px rgba(0, 0, 0, 0.4)",
-            }}
-          ></div>
-          <div className="flex flex-col gap-2 justify-center">
-            <p className="font-bold lg:text-2xl text-gray-800">Jon Endrick Babao</p>
-            <p className="font-semibold lg:text-2xl text-gray-800">22-01675</p>
+      <TextBoxPanel>
+        <div className="flex flex-col">
+          <div className="flex flex-row gap-8">
+            <div
+              className="rounded-3xl w-[150px] h-[150px] flex-shrink-0 flex items-center justify-center"
+              style={{
+                backgroundColor: "white",
+                boxShadow: "inset 0px 8px 20px rgba(0, 0, 0, 0.4)",
+              }}
+            ></div>
+            <div className="flex flex-col gap-2 justify-center">
+              <p className="font-bold lg:text-2xl text-gray-800">Jon Endrick Babao</p>
+              <p className="font-semibold lg:text-2xl text-gray-800">22-01675</p>
+              
+                <div className="flex flex-row items-center gap-8 mt-4 hidden sm:block">
+                  <div className="flex flex-col lg:text-lg">
+                    <p className="font-bold text-gray-600">Batangas State University</p>
+                    <p className="font-bold text-gray-600">CICS</p>
+                  </div>
+                </div>
             
-              <div className="flex flex-row items-center gap-8 mt-4 hidden sm:block">
-                <div className="flex flex-col lg:text-lg">
-                  <p className="font-bold text-gray-600">Batangas State University</p>
-                  <p className="font-bold text-gray-600">CICS</p>
-                </div>
-              </div>
-          
+            </div>
+            <button
+              onClick={closePopup}
+              className="self-start rounded -mt-2"
+            >
+              X
+            </button>
           </div>
-          <button
-            onClick={closePopup}
-            className="self-start rounded -mt-2"
-          >
-            X
-          </button>
         </div>
-      </div>
-      <div className="flex flex-row items-center gap-8 mt-4 sm:hidden">
-                <div className="flex flex-col lg:text-lg">
-                  <p className="font-bold text-gray-600">Batangas State University</p>
-                  <p className="font-bold text-gray-600">CICS</p>
-                </div>
-              </div>
-      
-    </TextBoxPanel>
+        <div className="flex flex-row items-center gap-8 mt-4 sm:hidden">
+          <div className="flex flex-col lg:text-lg">
+            <p className="font-bold text-gray-600">Batangas State University</p>
+            <p className="font-bold text-gray-600">CICS</p>
+          </div>
+        </div>
+            
+      </TextBoxPanel>
     </div>
     
   );
